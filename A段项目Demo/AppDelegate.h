d7a88f6274fecb2421fb958d43688b3f2d5b612c
lizhengwei <2501885827@qq.com>
2015-06-24 14:50:34 +0800

    Initial Commit

---
+++ A段项目Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  A段项目Demo
+//
+//  Created by laouhn on 15/6/24.
+//  Copyright (c) 2015年 laouhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ A段项目Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  A段项目Demo
+//
+//  Created by laouhn on 15/6/24.
+//  Copyright (c) 2015年 laouhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
